4660ad224c16da6e8b95a757440644d5818c2bf5
poro <poro@163.com>
2021-07-28 17:12:54 +0900

    123

---
+++ src/main/java/com/bilibili/App.java
@@ -0,0 +1,13 @@
+package com.bilibili;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/bilibili/domain/Test.java
@@ -0,0 +1,5 @@
+package com.bilibili.domain;
+
+public class Test {
+    private int id;
+}
